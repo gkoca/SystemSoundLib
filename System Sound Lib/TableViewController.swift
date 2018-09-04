d0c51497d692ecb62cc4cc8acee67813b116f87b
A. Gökhan KOCA <agkoca@netas.com.tr>
2018-09-04 14:42:12 +0300

    cleaunp

--- System Sound Lib/TableViewController.swift
+++ System Sound Lib/TableViewController.swift
@@ -10,29 +10,29 @@ import UIKit
 import AudioToolbox
 
 class TableViewController: UITableViewController {
-    
-    var soundList: [Sound] = [Sound]()
-    var filteredSoundList: [Sound] = [Sound]()
-    let searchController = UISearchController(searchResultsController: nil)
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        searchController.searchResultsUpdater = self
-        searchController.obscuresBackgroundDuringPresentation = false
-        searchController.searchBar.placeholder = "Search Sound"
-        navigationItem.searchController = searchController
-        definesPresentationContext = true
-        loadAudioFiles()
-    }
-
-    func loadAudioFiles() {
-        let fileManager = FileManager()
-        let directoryURL = URL(fileURLWithPath: "/System/Library/Audio/UISounds")
-        
+	
+	var soundList: [Sound] = [Sound]()
+	var filteredSoundList: [Sound] = [Sound]()
+	let searchController = UISearchController(searchResultsController: nil)
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		searchController.searchResultsUpdater = self
+		searchController.obscuresBackgroundDuringPresentation = false
+		searchController.searchBar.placeholder = "Search Sound"
+		navigationItem.searchController = searchController
+		definesPresentationContext = true
+		loadAudioFiles()
+	}
+	
+	func loadAudioFiles() {
+		let fileManager = FileManager()
+		let directoryURL = URL(fileURLWithPath: "/System/Library/Audio/UISounds")
+		
 		let enumarator = fileManager.enumerator(at: directoryURL, includingPropertiesForKeys: [.isDirectoryKey], options: .skipsHiddenFiles) { (url, err) -> Bool in
-            return true
-        }
+			return true
+		}
 		
 		if let fileEnumerator = enumarator {
 			fileEnumerator.forEach {
@@ -46,65 +46,65 @@ class TableViewController: UITableViewController {
 				}
 			}
 		}
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return isFiltering() ? filteredSoundList.count : soundList.count
-    }
-
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        if isFiltering() {
-            cell.textLabel?.text = filteredSoundList[indexPath.row].name
-            cell.detailTextLabel?.text = "sound Id: \(filteredSoundList[indexPath.row].soundId)"
-        } else {
-            cell.textLabel?.text = soundList[indexPath.row].name
-            cell.detailTextLabel?.text = "sound Id: \(soundList[indexPath.row].soundId)"
-        }
-        return cell
-    }
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if isFiltering() {
-            AudioServicesPlayAlertSound(filteredSoundList[indexPath.row].soundId)
-            print("playing sound id : \(filteredSoundList[indexPath.row].soundId)")
-        } else {
-            AudioServicesPlayAlertSound(soundList[indexPath.row].soundId)
-            print("playing sound id : \(soundList[indexPath.row].soundId)")
-        }
-        tableView.deselectRow(at: indexPath, animated: true)
-    }
+	}
+	
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+	}
+	
+	// MARK: - Table view data source
+	
+	override func numberOfSections(in tableView: UITableView) -> Int {
+		return 1
+	}
+	
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return isFiltering() ? filteredSoundList.count : soundList.count
+	}
+	
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+		if isFiltering() {
+			cell.textLabel?.text = filteredSoundList[indexPath.row].name
+			cell.detailTextLabel?.text = "sound Id: \(filteredSoundList[indexPath.row].soundId)"
+		} else {
+			cell.textLabel?.text = soundList[indexPath.row].name
+			cell.detailTextLabel?.text = "sound Id: \(soundList[indexPath.row].soundId)"
+		}
+		return cell
+	}
+	
+	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		if isFiltering() {
+			AudioServicesPlayAlertSound(filteredSoundList[indexPath.row].soundId)
+			print("playing sound id : \(filteredSoundList[indexPath.row].soundId)")
+		} else {
+			AudioServicesPlayAlertSound(soundList[indexPath.row].soundId)
+			print("playing sound id : \(soundList[indexPath.row].soundId)")
+		}
+		tableView.deselectRow(at: indexPath, animated: true)
+	}
 }
 
 //MARK: Search
 extension TableViewController: UISearchResultsUpdating {
-    
-    func updateSearchResults(for searchController: UISearchController) {
-        filterContentForSearchText(searchController.searchBar.text!)
-    }
-    
-    func searchBarIsEmpty() -> Bool {
-        return searchController.searchBar.text?.isEmpty ?? true
-    }
-    
-    func filterContentForSearchText(_ searchText: String, scope: String = "All") {
-        filteredSoundList = soundList.filter({ (sound: Sound) -> Bool in
-            return sound.name.lowercased().contains(searchText.lowercased())
-        })
-        tableView.reloadData()
-    }
-    
-    func isFiltering() -> Bool {
-        return searchController.isActive && !searchBarIsEmpty()
-    }
+	
+	func updateSearchResults(for searchController: UISearchController) {
+		filterContentForSearchText(searchController.searchBar.text!)
+	}
+	
+	func searchBarIsEmpty() -> Bool {
+		return searchController.searchBar.text?.isEmpty ?? true
+	}
+	
+	func filterContentForSearchText(_ searchText: String, scope: String = "All") {
+		filteredSoundList = soundList.filter({ (sound: Sound) -> Bool in
+			return sound.name.lowercased().contains(searchText.lowercased())
+		})
+		tableView.reloadData()
+	}
+	
+	func isFiltering() -> Bool {
+		return searchController.isActive && !searchBarIsEmpty()
+	}
 }
